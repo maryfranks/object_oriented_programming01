185799eba3d6ada68fca0b713b8454a95da91c73
maryfranks <maryfranks@gmail.com>
2017-06-30 14:43:32 -0400

    added deposit, withdraw, and gain interest methods

--- bank_account.rb
+++ bank_account.rb
@@ -5,5 +5,18 @@ class BankAccount
     @interest_rate = interest_rate
   end
 
-
+  def deposit(deposit_amount)
+    @balance = deposit_amount + @balance
+  end
+  def withdraw(withdraw_amount)
+    @balance = @balance - withdraw_amount
+  end
+  def gain_interest
+    @balance = @balance + (@balance * @interest_rate)
+  end
 end
+
+
+# checking = BankAccount.new(100, 0.05)
+
+# checking.deposit(50)
