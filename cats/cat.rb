94f84c51a4703d42378b42a110fc3b2445511867
maryfranks <maryfranks@gmail.com>
2017-06-30 14:49:46 -0400

    created class Cat with three attributes

---
+++ cats/cat.rb
@@ -0,0 +1,10 @@
+class Cat
+
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+  end
+
+end
+# tabby = Cat.new("Tabby", "salmon", "8")
