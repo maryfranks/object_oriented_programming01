ada3bb575672eec70dca1109e0bc20be386babe9
maryfranks <maryfranks@gmail.com>
2017-06-30 17:54:29 -0400

    finished paperboy assignment

--- paperboy_game/paperboy.rb
+++ paperboy_game/paperboy.rb
@@ -13,25 +13,35 @@ class PaperBoy
     quota_papers = 0
     above_quota_papers = 0
     # calculate number of quota papers and over quota papers
-    if papers > @quota
+    if papers >= @quota
+      # puts "you delivered #{papers} and you met your quota!"
       quota_papers = @quota
       above_quota_papers = papers - quota_papers
     else
+      # puts "you delivered #{papers} and you did not meet your quota"
       quota_papers = papers
       above_quota_papers = 0
+      # @earnings -= 2
     end
     # calculate earnings from quota papers and above quota papers
     quota_papers_earnings = quota_papers * 0.25
-    above_quota_papers_earnings = above_quota_papers * 0.5
-    @earnings = quota_papers_earnings + above_quota_papers_earnings
+    above_quota_papers_earnings = above_quota_papers * 0.50
+    @earnings = @earnings + (quota_papers_earnings + above_quota_papers_earnings)
+    @quota = 50 + (@experience / 2)
+    # puts "You have $#{@earnings}, #{@experience} experience, and your quota is #{@quota}"
+  end
 
-    puts "You have $#{@earnings} and #{@experience} experience"
+  def report
+    puts "I'm #{@name}, I've delivered #{@experience} papers and I've earned $#{@earnings} so far!"
   end
 
 end
 
 jack = PaperBoy.new("Jack")
 
+# jack.deliver(100, 200)
 jack.deliver(50, 100)
-jack.deliver(100, 200)
-jack.deliver(100, 300)
+jack.report
+jack.deliver(50, 100)
+jack.report
+# jack.deliver(50, 75)
